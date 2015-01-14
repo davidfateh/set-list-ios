bdf0f79c9f4a30caea43354caa23c7d50f30617b
afriedman94 <andrewfriedman@Andrews-MacBook-Pro.local>
2015-01-13 19:56:49 -0500

    Initial Commit

---
+++ TheSetListTests/TheSetListTests.m
@@ -0,0 +1,40 @@
+//
+//  TheSetListTests.m
+//  TheSetListTests
+//
+//  Created by Andrew Friedman on 1/13/15.
+//  Copyright (c) 2015 Andrew Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TheSetListTests : XCTestCase
+
+@end
+
+@implementation TheSetListTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
